317fd52908b735f561c3a4c78fbb22950f2ec992
Kazumasa Utashiro <kaz@utashiro.com>
2022-01-28 12:51:17 +0900

    minil new App::Greple::jq

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Greple::jq - It's new $module
+
+# SYNOPSIS
+
+    use App::Greple::jq;
+
+# DESCRIPTION
+
+App::Greple::jq is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/Greple/jq.pm
@@ -0,0 +1,39 @@
+package App::Greple::jq;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::jq - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Greple::jq;
+
+=head1 DESCRIPTION
+
+App::Greple::jq is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
